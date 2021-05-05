cefa59cd5f1b73e53ecbd6ba0a374e05ca514801
Alexander Semenov <modern.rising@outlook.com>
2021-05-05 13:39:08 +0300

    step 8

--- src/js/script.js
+++ src/js/script.js
@@ -84,6 +84,77 @@ $(document).ready(function(){
           $(".overlay, #order").fadeIn("slow");
         });
       });
+
+      function valideForms (form) {
+        $(form).validate({
+          rules: {
+            name: {
+              required: true,
+              minlength: 2
+            },
+            phone: "required",
+            email: {
+              required:true,
+              email:true
+            }
+          },
+  
+          messages: {
+            name:  {
+              required: "Пожалуйста введите свое имя",
+              minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+  
+            phone: "Пожалуйста введите свой номер телефона",
+            email: {
+              required: "Пожалуйста введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+          }
+  
+        });
+      }
+
+      valideForms("#consultation-form");
+      valideForms("#consultation form");
+      valideForms("#order form");
+
+      $("input[name=phone]").mask("+7 (999) 999-9999");
+
+      $("form").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function(){
+          $(this).find("input").val("");
+          $("#consultation, #order").fadeOut();
+          $(".overlay, #thanks").fadeIn("slow");
+
+
+          $("form").trigger("reset");
+        });
+        return false;
+      });
+
+      //smooth scroll
+
+      $(window).scroll(function(){
+        if ($(this).scrollTop()>1600) {
+          $(".pageup").fadeIn();
+
+        } else {
+          $(".pageup").fadeOut();
+        }
+      });
+
+      $("a[href^='#up']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+new WOW().init();
     });
     })(jQuery);
 
